53c117fb86ea009161e01355d050007cb4aad7dd
Shayan Poursadeghi <sh.psadeghi@gmail.com>
2025-01-08 14:56:33 +0330

    provider logic added

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,3 +1,4 @@
+import 'package:employee_attendance_app/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -12,4 +13,43 @@ class AuthService extends ChangeNotifier{
     _isLoading = value;
     notifyListeners();
   }
+
+  Future registerEmployee(String email, String password, BuildContext context) async{
+    try {
+      setIsLoading = true;
+      if (email == "" || password == "") {
+        throw("All fields are required!");
+      }
+      final AuthResponse response = await _supabaseClient.auth.signUp(email: email ,password: password);
+      Utils.showSnackBar(context, "Success! You can Login now", color: Colors.green);
+      Navigator.pop(context);
+      setIsLoading = false;
+    } catch (e) {
+      setIsLoading = false;
+      Utils.showSnackBar(context, e.toString(), color: Colors.red);
+    }
+  }
+
+  Future loginEmployee(String email, String password, BuildContext context) async{
+    try {
+      setIsLoading = true;
+      if (email == "" || password == "") {
+        throw("All fields are required!");
+      }
+      final AuthResponse response = await _supabaseClient.auth.signInWithPassword(email: email ,password: password);
+      Utils.showSnackBar(context, "You Login Successfully!", color: Colors.green);
+      //Navigator.pop(context);
+      setIsLoading = false;
+    } catch (e) {
+      setIsLoading = false;
+      Utils.showSnackBar(context, e.toString(), color: Colors.red);
+    }
+  }
+
+  Future signOut() async{
+    await _supabaseClient.auth.signOut();
+    notifyListeners();
+  }
+
+  User? get currentUser => _supabaseClient.auth.currentUser; //Returns the user data, if there is a logged in user
 }
