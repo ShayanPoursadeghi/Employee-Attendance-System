1d58e7f1dca521a0ebeb926283889eb0b136671f
Shayan Poursadeghi <sh.psadeghi@gmail.com>
2025-01-18 10:09:19 +0330

    bottom Nav Bar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:employee_attendance_app/screens/login_screen.dart';
+import 'package:employee_attendance_app/screens/splash_screen.dart';
 import 'package:employee_attendance_app/services/auth_service.dart';
 import 'package:employee_attendance_app/services/db_service.dart';
 import 'package:flutter/material.dart';
@@ -35,7 +36,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const LoginScreen(),
+        home: const SplashScreen(),
       ),
     );
   }
