3e755be5e452f6c7f22d29cb7ab88156e6bf378e
Shayan Poursadeghi <sh.psadeghi@gmail.com>
2025-01-19 12:22:35 +0330

    getUserData func added to db_service

--- lib/services/db_service.dart
+++ lib/services/db_service.dart
@@ -1,26 +1,39 @@
 import 'dart:math';
 import 'package:employee_attendance_app/constants/constants.dart';
+import 'package:employee_attendance_app/models/user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class DbService extends ChangeNotifier{
+class DbService extends ChangeNotifier {
   final SupabaseClient _supabaseClient = Supabase.instance.client;
+  UserModel? userModel;
 
-  String generatedRandomEmployeeId(){
+  String generatedRandomEmployeeId() {
     final random = Random();
     const allchars = "abcdefABCDEF0123456789";
-    final randomString = List.generate(8, (index) => allchars[random.nextInt(allchars.length)]).join();
+    final randomString =
+        List.generate(8, (index) => allchars[random.nextInt(allchars.length)])
+            .join();
     return randomString;
   }
 
-  Future insertNewUser(String email, var id) async{
+  Future insertNewUser(String email, var id) async {
     await _supabaseClient.from(Constants.employeeTable).insert({
-'id': id,
-'name': '',
-'email': email,
-'employee_id': generatedRandomEmployeeId(),
-'department': null,
+      'id': id,
+      'name': '',
+      'email': email,
+      'employee_id': generatedRandomEmployeeId(),
+      'department': null,
     });
+  }
 
+  Future<UserModel> getUserData() async {
+    final userData = await _supabaseClient
+        .from(Constants.employeeTable)
+        .select()
+        .eq('id', _supabaseClient.auth.currentUser!.id)
+        .single();
+        userModel = UserModel.fromJson(userData);
+        return userModel!;
   }
 }
