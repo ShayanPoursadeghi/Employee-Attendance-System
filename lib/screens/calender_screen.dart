934bb3e47bcd39ee52316477dd34fd19b3e61555
Shayan Poursadeghi <sh.psadeghi@gmail.com>
2025-01-28 11:43:46 +0330

    attendance history screen phase 2

--- lib/screens/calender_screen.dart
+++ lib/screens/calender_screen.dart
@@ -1,4 +1,8 @@
+import 'package:employee_attendance_app/services/attendance_service.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:simple_month_year_picker/simple_month_year_picker.dart';
 
 class CalenderScreen extends StatefulWidget {
   const CalenderScreen({super.key});
@@ -10,10 +14,57 @@ class CalenderScreen extends StatefulWidget {
 class _CalenderScreenState extends State<CalenderScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Calender"),
-      ),
+    final attendanceService = Provider.of<AttendanceService>(context);
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.centerLeft,
+          margin: const EdgeInsets.only(left: 20, top: 60, bottom: 10),
+          child: const Text(
+            "My Attendance",
+            style: TextStyle(fontSize: 25),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              attendanceService.attendanceHistoryMonth,
+              style: const TextStyle(fontSize: 25),
+            ),
+            OutlinedButton(
+                onPressed: () async {
+                  final selectedDate =
+                      await SimpleMonthYearPicker.showMonthYearPickerDialog(
+                          context: context, disableFuture: true);
+                  String pickMonth =
+                      DateFormat('MMMM yyyy').format(selectedDate);
+                  attendanceService.attendanceHistoryMonth = pickMonth;
+                },
+                child: const Text("Pick a month"))
+          ],
+        ),
+        Expanded(
+            child: FutureBuilder(
+                future: attendanceService.getAttendanceHistory(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasData) {
+                    if (snapshot.data.length > 0) {
+                    } else {
+                      return const Center(
+                        child: Text(
+                          "No DataAvailable",
+                          style: TextStyle(fontSize: 25),
+                        ),
+                      );
+                    }
+                  }
+                  return const LinearProgressIndicator(
+                    backgroundColor: Colors.white,
+                    color:  Colors.grey,
+                  );
+                }))
+      ],
     );
   }
 }
